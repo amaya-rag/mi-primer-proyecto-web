7b167f0dc23ba2e01e34833b75c9aad2fec64452
miguel-amaya <antonyamaya582@gmail.com>
2026-09-22 08:40:32 -0600

    Adapta el proyecto a desarrollo de software

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-function cambiarBombilla() {
-
-    let imagen = document.getElementById("bombilla");
-    let boton = document.getElementById("boton");
-
-    if (boton.innerText === "Encender") {
-
-        imagen.src = "Bombilla2.gif.gif";
-        boton.innerText = "Apagar";
-
-    } else {
-
-        imagen.src = "Bombilla.gif.gif";
-        boton.innerText = "Encender";
-
-    }
-}
+function mostrarMensaje() { let mensaje = document.getElementById("mensaje"); mensaje.textContent = "¡Estoy aprendiendo a programar con JavaScript!"; }
